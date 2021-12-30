8ef90bb6efd11d87b19601604b62962b0a61dad6
GabrielAlvesdeOliveira <alvesg2003@gmail.com>
2021-12-30 13:05:17 -0300

    Creating Project

---
+++ frontend/src/components/home/home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Main from "../template/main";
+
+export default props => 
+    <Main icon="home" title="Inicio"
+     subtitle="Segundo Projeto do capitulo de React." >
+         <div className='display-4'>Bem vindo</div>
+         <hr />
+         <p className="mb-0">sistema para exemplificar o cadastro, desenvolvido em react</p>
+    </Main>
---
+++ frontend/src/components/template/footer.jsx
@@ -0,0 +1,10 @@
+import './footer.css'
+import React from "react";
+
+export default props => 
+<aside className='footer'>
+    <span>
+        Desenvolvido com <i className='fa fa-heart text-danger'></i> por 
+        <strong> cod<span className='text-danger'>3</span>r</strong>
+    </span>
+</aside> 
